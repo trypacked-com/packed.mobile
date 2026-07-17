2fbb2e0c14535f2b568a41b91632f4aadfa07346
joshbatley <jbatley12@gmail.com>
2026-07-17 13:31:07 +0100

    feat: add packed checkbox

---
+++ components/ui/checkbox.tsx
@@ -0,0 +1,47 @@
+import { Icon } from '@/components/ui/icon';
+import { cn } from '@/lib/utils';
+import * as CheckboxPrimitive from '@rn-primitives/checkbox';
+import { Check } from 'lucide-react-native';
+import { Platform } from 'react-native';
+
+const DEFAULT_HIT_SLOP = 24;
+
+function Checkbox({
+  className,
+  checkedClassName,
+  indicatorClassName,
+  iconClassName,
+  ...props
+}: React.ComponentProps<typeof CheckboxPrimitive.Root> & {
+  checkedClassName?: string;
+  indicatorClassName?: string;
+  iconClassName?: string;
+}) {
+  return (
+    <CheckboxPrimitive.Root
+      className={cn(
+        'border-border bg-background size-4 shrink-0 rounded-md border shadow-card',
+        Platform.select({
+          web: 'focus-visible:border-border-brand focus-visible:ring-ring/40 aria-invalid:ring-destructive/20 aria-invalid:border-destructive peer cursor-default outline-none transition-shadow focus-visible:ring-[3px] disabled:cursor-not-allowed',
+          native: 'overflow-hidden',
+        }),
+        props.checked && cn('border-brand', checkedClassName),
+        props.disabled && 'opacity-50',
+        className
+      )}
+      hitSlop={DEFAULT_HIT_SLOP}
+      {...props}>
+      <CheckboxPrimitive.Indicator
+        className={cn('bg-brand h-full w-full items-center justify-center', indicatorClassName)}>
+        <Icon
+          as={Check}
+          size={12}
+          strokeWidth={Platform.OS === 'web' ? 2.5 : 3.5}
+          className={cn('text-on-brand', iconClassName)}
+        />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+}
+
+export { Checkbox };
